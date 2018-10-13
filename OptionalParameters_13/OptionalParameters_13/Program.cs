58fe7313b475822de1fd33a7297a1fa5c29af619
Nibbly <34423578+Nibbly@users.noreply.github.com>
2018-10-13 12:12:26 +0200

    Adds optional parameters

---
+++ OptionalParameters_13/OptionalParameters_13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintData("John");
+
+            Console.ReadKey();
+        }
+
+        // Alle nicht optionalen Parameter müssen in der Methodensignatur zuerst angegeben werden
+
+        static void PrintData(string firstName, string lastName = null, int age = 0)
+        {
+            Console.WriteLine("{0} {1} is {2} years old.", firstName, lastName, age);
+        }
+
+        //Methoden müssen für jeden benötigten Fall überladen werden -> Lösung optionale Paramter
+
+        //static void PrintData(string firstName, string lastName, int age)
+        //{
+        //    Console.WriteLine("{0} {1} is {2} years old.", firstName, lastName, age);
+        //}
+
+        //static void PrintData(string firstName, string lastName)
+        //{
+        //    PrintData(firstName, lastName, 0);
+        //}
+
+        //static void PrintData(string firstName)
+        //{
+        //    PrintData(firstName, null, 0);
+        //}
+    }
+}
