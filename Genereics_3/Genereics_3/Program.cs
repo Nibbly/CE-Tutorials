18059291b908c26553cd1dca8b35b51b08f4d62f
Nibbly <34423578+Nibbly@users.noreply.github.com>
2018-10-13 11:46:37 +0200

    Adds generic list sample

--- Genereics_3/Genereics_3/Program.cs
+++ Genereics_3/Genereics_3/Program.cs
@@ -10,18 +10,14 @@ namespace Genereics_3
     {
         static void Main(string[] args)
         {
-            var res = new Result<int, string> { Success = false, Data = 100, Data2 = "Bob" };
-            var res2 = new Result<string, bool> { Success = true, Data = "John", Data2 = true };
+            var res = new Result<int, string> { Success = false, Data = 100, List = new List<string> { "Hello", "Sir" } };
+            var res2 = new Result<string, int> { Success = true, Data = "John", List = new int[] { 2, 3, 4, 5 } };
 
-            Print(res);
-            Print(res2);
+            var printer = new Printer();
+            printer.Print(res);
+            printer.Print(res2);
 
             Console.ReadKey();
-        }
-
-        public static void Print<T, U>(Result<T, U> res)
-        {
-            Console.WriteLine("Success: {0}\tData: {1}\tData2: {2}", res.Success, res.Data, res.Data2);
         }
     }
 
@@ -29,6 +25,25 @@ namespace Genereics_3
     {
         public bool Success { get; set; }
         public T Data { get; set; }
-        public U Data2 { get; set; }
+        public IEnumerable<U> List { get; set; }
+    }
+
+    public class Printer
+    {
+        public void Print<T, U>(Result<T, U> res)
+        {
+            Console.Write("Success: {0}", res.Success);
+            Console.Write("\tData: {0}", res.Data);
+            Console.Write("\t Liste: ");
+
+            foreach (var item in res.List)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\tList-Type: {0}", typeof(U));
+
+            Console.WriteLine();
+        }
     }
 }
