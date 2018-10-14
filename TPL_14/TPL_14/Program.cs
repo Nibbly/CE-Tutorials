9e6015c09194286bae9d335f3b633e5598332c6e
Nibbly <34423578+Nibbly@users.noreply.github.com>
2018-10-14 09:46:14 +0200

    Simple Task.Start

---
+++ TPL_14/TPL_14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Task(() => DoSomething(1, 2000));
+            t1.Start();
+            var t2 = new Task(() => DoSomething(2, 1000));
+            t2.Start();
+            var t3 = new Task(() => DoSomething(3, 4000));
+            t3.Start();
+
+
+            Console.WriteLine("Press key to continue");
+            Console.ReadKey();
+        }
+
+        static void DoSomething(int id, int sleeptime)
+        {
+            Console.WriteLine("Task {0} has started!", id);
+            Thread.Sleep(sleeptime);
+            Console.WriteLine("Task {0} has completed!", id);
+        }
+    }
+}
