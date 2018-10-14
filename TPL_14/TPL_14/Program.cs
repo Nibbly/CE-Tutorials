5b2d3207106f24d148a617f5536bc35106bf5eaa
Nibbly <34423578+Nibbly@users.noreply.github.com>
2018-10-14 10:31:43 +0200

    Using Cancellation-Tokens

--- TPL_14/TPL_14/Program.cs
+++ TPL_14/TPL_14/Program.cs
@@ -11,26 +11,42 @@ namespace TPL_14
     {
         static void Main(string[] args)
         {
-            var nums = new List<int> { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+            var source = new CancellationTokenSource();
+            try
+            {
+                var t1 = Task.Factory.StartNew(() => DoSomething(1, 2000, source.Token)).ContinueWith((prevTask) => DoSomethingElse(2, 3000, source.Token));
+                source.Cancel();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
 
-            // Parallel.ForEach / .For beendet erst seine Operationen bevor es weiter geht
-            Parallel.ForEach(nums, (i) => { Console.WriteLine("i: {0}\ti^2: {1}", i, i * i);});
-
-            Parallel.For(0, 100, (i) => Console.WriteLine(i));
 
             Console.WriteLine("Press key to continue");
             Console.ReadKey();
         }
 
-        static void DoSomething(int id, int sleeptime)
+        static void DoSomething(int id, int sleeptime, CancellationToken token)
         {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Cancellation is requested {0}", id);
+                token.ThrowIfCancellationRequested();
+            }
+
             Console.WriteLine("Task {0} has started!", id);
             Thread.Sleep(sleeptime);
             Console.WriteLine("Task {0} has completed!", id);
         }
 
-        static void DoSomethingElse(int id, int sleeptime)
+        static void DoSomethingElse(int id, int sleeptime, CancellationToken token)
         {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Cancellation is requested {0}", id);
+                token.ThrowIfCancellationRequested();
+            }
             Console.WriteLine("Task {0} has started more work!", id);
             Thread.Sleep(sleeptime);
             Console.WriteLine("Task {0} has completed more work!", id);
